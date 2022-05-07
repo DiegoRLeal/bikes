f340b6a69cf33533dd8be7dab40f7094cf2db6b9
Diego Leal <diego.leal@brasas.com>
2022-05-07 17:01:27 -0300

    Implement Index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Offer.destroy_all
+10.times do
+  Offer.create!(
+    bike: ['Abus (42)', 'adidas (6)', 'Nalgene (2)', 'NAMEDSport (4)', 'Oakley (28)'  Panaracer (10) Park (7)
+      Radial (6) Raleigh (71)
+      ].sample,
+    localization: Faker::Address.city,
+    price: rand(20..100),
+    bike_condition: ['new', 'used', 'under maintenance'].sample
+  )
+end
