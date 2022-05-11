280cb71c14a6ef84bb6b0a5a8d333026f75c2b73
Fernanda Santos <fevsantos@gmail.com>
2022-05-10 21:34:28 -0300

    Adicionei Available e metodos no Offer

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -2,4 +2,24 @@ class OffersController < ApplicationController
   def index
     @offers = Offer.all
   end
+
+  def new
+    @offer = Offer.new
+  end
+
+  def create
+    @offer = Offer.new(offer_params)
+    end
+
+  def show
+    @offer = Offer.find(params[:id]
+  end
+  
+  private
+
+  def offer_params
+    params.require(:offer).permit(:bike, :localization, :price, :bike_condition)
+  end
+
+
 end
