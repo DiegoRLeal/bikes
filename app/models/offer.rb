dbf753b42f408f41cb5478997a31717a99f67586
Diego Leal <diego.leal@brasas.com>
2022-05-11 12:04:43 -0300

    offer show

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,2 +1,5 @@
 class Offer < ApplicationRecord
+  validates :bike, :localization, :price, :bike_condition, presence: true
+  validates :price, numericality: { only_integer: true }
+  has_one_attached :image
 end
