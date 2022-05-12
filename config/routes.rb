a1bf1b27cb3671f7e5da540931de59ce20fc52fc
Diego Leal <diego.leal@brasas.com>
2022-05-12 19:35:18 -0300

    Implement user_cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :products
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :products do
+    resources :user_carts, only: [:new, :create]
+  end
+  resources :user_carts, only: :index
 end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
